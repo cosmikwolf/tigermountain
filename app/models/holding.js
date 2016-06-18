e7ac7ae91518dda8db08a56b7abda686beb679a0
Tenkai Kariya <tenkai@zetaohm.com>
2016-06-17 17:39:47 -0700

    Rails server is working! wee!

--- app/models/holding.js
+++ app/models/holding.js
@@ -1,10 +1,10 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { belongsTo, hasMany } from 'ember-data/relationships';
+import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   issue: belongsTo('issue'),
-  //owner: belongsTo('owner'),
+  owner: belongsTo('owner'),
   querydate: attr(),
   ticker: attr(),
   companyname: attr(),
--- app/models/owner.js
+++ app/models/owner.js
@@ -1,7 +1,24 @@
 import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-
+    holdings: hasMany(),
+    portfoliovalue: attr(),
+    ownername: attr(),
+    street1: attr(),
+    street2: attr(),
+    street3: attr(),
+    city: attr(),
+    state: attr(),
+    zip: attr(),
+    phone: attr(),
+    dcn: attr(),
+    newpositions: attr(),
+    increasedpositions: attr(),
+    decreasedpositions: attr(),
+    soldoutpositions: attr(),
+    filerid: attr(),
+    issuesheld: attr(),
+    cik: attr(),
 });
