51e5f7cb2cf3fd028ee8823e8af6ffa3569d2f04
Tenkai Kariya <tenkai@zetaohm.com>
2016-06-18 14:35:26 -0700

    restadapter stufff
    Signed-off-by: Tenkai Kariya <tenkai@zetaohm.com>

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default JSONAPIAdapter.extend({
 
-  host: 'https://tiger-mountain-api.herokuapp.com'
+  host: 'https://tiger-mountain-api.herokuapp.com';
 
   pathForType: function(type) {
     return Ember.String.pluralize(Ember.String.underscore(type));
